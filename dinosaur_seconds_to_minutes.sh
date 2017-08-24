12d943e7a10e9e4ec384c86d73d06c18f0c019a8
Jakob Willforss <jakob.willforss@hotmail.com>
2017-08-24 09:36:12 +0200

    Small awk script for conversion of Dinosaur time format to minutes

---
+++ dinosaur_seconds_to_minutes.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ${0##*/} input.tsv output.tsv"
+    exit 1
+fi
+
+input=$1
+output=$2
+
+grep -v "^#" ${input} | awk -v OFS='\t' '{if (NR > 1) $3=$3/60; print}' > ${output}
+
